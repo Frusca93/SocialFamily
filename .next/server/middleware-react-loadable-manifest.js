705112fc401021e00f2d5dfe886f198539638bd0
Frusca93 <mauro.dicarlo1993@gmail.com>
2025-08-12 11:03:15 +0200

    bug fix

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"components\\\\PostCard.tsx -> ./CommentsModal\":{\"id\":\"components\\\\PostCard.tsx -> ./CommentsModal\",\"files\":[\"static/chunks/_app-pages-browser_src_components_CommentsModal_tsx.js\"]}}"
